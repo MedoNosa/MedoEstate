73a90d2f6a314c66102a085569a9908901dd5514
MedoNosa <mohameddahman351@gmail.com>
2025-07-20 12:32:26 +0300

    edit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,27 +1,49 @@
-import { verifyWebhook } from '@clerk/nextjs/webhooks'
-import { NextRequest } from 'next/server'
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
 
-export async function POST(req: NextRequest) {
-  try {
-    const evt = await verifyWebhook(req)
+export async function POST(req) {
+  const CLERK_WEBHOOK_SIGNING_SECRET = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
+  if (!CLERK_WEBHOOK_SIGNING_SECRET) {
+    throw new Error('CLERK_WEBHOOK_SIGNING_SECRET is not set in environment variables');
+  }
+  const wh = new Webhook(CLERK_WEBHOOK_SIGNING_SECRET);
 
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+    const svix_signature = headerPayload.get('svix-signature');
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = evt.data
-    const eventType = evt.type
-    if (evt.type === 'user.created') {
-  console.log('userCreated')
-}
- if (evt.type === 'user.deleted') {
-  console.log('userDeleted')
-}
- if (evt.type === 'user.updated') {
-  console.log('userUpdated')
-}
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+        return new Response('Missing required headers', { status: 400 });
+    }
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+    let evt;
+    try {
+        evt = wh.verify(body,{
+            'svix-id': svix_id,
+            'svix-timestamp': svix_timestamp,
+            'svix-signature': svix_signature
+        });
+    } catch (err) {
+        console.error('Webhook verification failed:', err);
+        return new Response('Invalid webhook signature', { status: 400 });
+    }
+    const { id } = evt.data;
+    const eventType = evt.type;
+
+    if (eventType === 'user.created') {
+        console.log("User created ");
+    }
+
+    if (eventType === 'user.deleted') {
+        console.log("User deleted ");
+    }
+    if (eventType === 'user.updated') {
+        console.log("User updated ");
+    }
 
     return new Response('Webhook received', { status: 200 })
-  } catch (err) {
-    console.error('Error verifying webhook:', err)
-    return new Response('Error verifying webhook', { status: 400 })
-  }
+  
 }
